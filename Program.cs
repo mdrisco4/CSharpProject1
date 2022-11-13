da04739efe1effbbd9a563436364f39ba277fd63
michaeldriscoll <michaelndriscoll81@gmail.com>
2022-11-13 17:18:09 -0500

    figurng out basics

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+namespace program1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string myName = args[0];
+            Console.WriteLine("Let's write C# code! I am" + myName);
+        }
+    }
+}
